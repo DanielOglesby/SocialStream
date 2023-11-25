5185da9286d601c02192d791e6a8abbfd6cc316d
Daniel Oglesby <dgo4man1@gmail.com>
2023-11-25 14:14:11 -0600

    feat: add youtube player component

---
+++ src/routes/stream/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Youtube from '../../components/YouTube.svelte';
+	import Layout from '../+layout.svelte';
+
+	let player;
+
+	const toggle = () => {
+		console.log('changing video id');
+		player.loadVideoById('lS7vwED8eC8');
+	};
+</script>
+
+<div class="youtube-player">
+	<Youtube bind:player />
+</div>
+
+<button on:click={toggle}>change video</button>
+
+<style>
+	.youtube-player {
+		width: 600px;
+		aspect-ratio: 16/9;
+	}
+</style>
