59ce034e326a6910b2e09305e2e83cc63bcc19a8
Daniel Oglesby <dgo4man1@gmail.com>
2023-11-30 16:53:30 -0600

    feat: delete favicon

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,15 @@ import { initializeApp, getApps, getApp, deleteApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-let firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+const firebaseConfig = {
+  apiKey: "AIzaSyDMyno4Ju3SmpmOZU1ouj8xn-rY8pMJmTc",
+  authDomain: "socialstream-5b704.firebaseapp.com",
+  databaseURL: "https://socialstream-5b704-default-rtdb.firebaseio.com",
+  projectId: "socialstream-5b704",
+  storageBucket: "socialstream-5b704.appspot.com",
+  messagingSenderId: "727474452664",
+  appId: "1:727474452664:web:4b8d7ed52140f1ee625c4d",
+  measurementId: "G-ES3F0EED44"
 };
 
 let firebaseApp;
