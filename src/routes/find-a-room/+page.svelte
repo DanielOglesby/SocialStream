15b37400c581315cc551d4f2499dae70b80a17f4
Daniel Oglesby <dgo4man1@gmail.com>
2023-12-01 15:44:32 -0600

    fix: add logic to make sure that no duplicate currentVideo docs get created

--- src/routes/find-a-room/+page.svelte
+++ src/routes/find-a-room/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import dayjs from 'dayjs';
 	import { db } from '../../firebase';
-	import { doc, setDoc, collection, addDoc } from 'firebase/firestore';
+	import { doc, setDoc, collection, addDoc, getDocs } from 'firebase/firestore';
 	import { authStore } from '../../stores/authStore';
 
 	let roomName = '';
@@ -41,15 +41,24 @@
 				message: 'Welcome to the room, ' + currentUser?.email + '!',
 				color: 'blue'
 			};
+			// Create a reference to the 'currentVideo' subcollection within the room
 			const currentVideoCollectionRef = collection(roomDocRef, 'currentVideo');
-			const newVideo = {
-				videoId: 'https://www.youtube.com/watch?v=Z7Gp25HHOO0',
-				timestamp: dayjs().format('MMMM D, YYYY @ h:mm A')
-			};
-			await addDoc(currentVideoCollectionRef, {
-				videoId: newVideo,
-				timestamp: dayjs().format('MMMM D, YYYY @ h:mm A')
-			});
+
+			// Check if the 'currentVideo' subcollection is empty
+			const currentVideoSnapshot = await getDocs(currentVideoCollectionRef);
+			if (currentVideoSnapshot.empty) {
+				// If the subcollection is empty, create a new video object
+				const newVideo = {
+					videoId: 'https://www.youtube.com/watch?v=Z7Gp25HHOO0',
+					timestamp: dayjs().format('MMMM D, YYYY @ h:mm A')
+				};
+
+				// Add a new document with the new video data to the 'currentVideo' subcollection
+				await addDoc(currentVideoCollectionRef, {
+					videoId: newVideo,
+					timestamp: dayjs().format('MMMM D, YYYY @ h:mm A')
+				});
+			}
 			await addDoc(messagesCollectionRef, newMessage);
 
 			window.location.href = `/stream/${roomName}`;
