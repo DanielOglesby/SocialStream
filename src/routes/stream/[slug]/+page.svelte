a06965a49fd0252a5a07e7f6db2b19d01d93f3e8
Daniel Oglesby <dgo4man1@gmail.com>
2023-11-30 22:10:06 -0600

    chore: update imports

--- src/routes/stream/[slug]/+page.svelte
+++ src/routes/stream/[slug]/+page.svelte
@@ -2,6 +2,21 @@
 	import Youtube from '../../../components/YouTube.svelte';
 	import Chat from '../../../components/Chat.svelte';
 	import { trimURL } from '../../../utils/helpers';
+	import { db } from '../../../firebase';
+	import {
+		doc,
+		setDoc,
+		collection,
+		addDoc,
+		onSnapshot,
+		orderBy,
+		getDocs,
+		getDoc,
+		query,
+		limit,
+		updateDoc
+	} from 'firebase/firestore';
+	import { onMount } from 'svelte';
 
 	let player;
 	let videoSlug: string;
