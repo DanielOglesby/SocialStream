435018cc76bcd1fd5eea99a303512ef6c54a2672
Daniel Oglesby <dgo4man1@gmail.com>
2023-12-22 16:25:39 -0600

    feat: create about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,37 @@
+<main class="container mx-auto">
+	<h1 class="text-4xl font-bold mb-4">What is SocialStream?</h1>
+	<p class="text-lg mb-4">A streaming service to watch YouTube videos with friends in real time.</p>
+
+	<h2 class="text-2xl font-bold mb-2">Features</h2>
+	<ul class="list-disc ml-6 mb-4">
+		<li>Real-time streaming of YouTube videos</li>
+		<li>Watch videos with friends</li>
+		<li>Chat with friends while watching</li>
+		<li>Sync playback across devices</li>
+	</ul>
+
+	<h2 class="text-2xl font-bold mb-2">How it works</h2>
+	<p class="mb-4">
+		SocialStream allows you to create virtual rooms where you can invite your friends to watch
+		YouTube videos together. Here's how it works:
+	</p>
+	<ol class="list-decimal ml-6 mb-4">
+		<li>Create a room using a unique name</li>
+		<li>Other users can join by typing in the same name</li>
+		<li>Any user can then input a YouTube URL into the text bar</li>
+		<li>Any user can then change the time for all other users using the timestamp slider</li>
+		<li>Participants can chat with each other while watching the video</li>
+		<li>
+			IMPORTANT: Playback is currently only synchronized via the timestamp slider, so if one user
+			pauses the video or changes the time using the YouTube iframe, it won't update for the other
+			users. You must use the timestamp slider.
+		</li>
+	</ol>
+	<a href="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+		>Get Started</a
+	>
+</main>
+
+<style>
+	/* Add your custom styles here */
+</style>
